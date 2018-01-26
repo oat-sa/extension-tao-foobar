f7587cf08f84f75db73678fec7a16547ffe632bf
Christophe Noël <christophe@taotesting.com>
2018-01-26 15:33:35 +0100

    completed the feature

--- views/js/controller/Foo/index.js
+++ views/js/controller/Foo/index.js
@@ -7,6 +7,7 @@ define([], function(){
             window.console.log('foobar baz noz', arguments);
             window.console.warn('Ooops');
             window.console.log('that is a cool new fiture');
+            window.console.log('now the fiture is complete');
         }
     };
 });
