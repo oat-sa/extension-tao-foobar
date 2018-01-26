5a15d716ad7afba8eeab4f35faf51344cac5c870
Christophe Noël <christophe@taotesting.com>
2018-01-26 15:33:08 +0100

    added new feature

--- views/js/controller/Foo/index.js
+++ views/js/controller/Foo/index.js
@@ -6,6 +6,7 @@ define([], function(){
             //foo starts
             window.console.log('foobar baz noz', arguments);
             window.console.warn('Ooops');
+            window.console.log('that is a cool new fiture');
         }
     };
 });
