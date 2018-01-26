3bd43b494b7aedcd09f2d96123cd6a24846dc758
Christophe Noël <christophe@taotesting.com>
2018-01-26 15:41:29 +0100

    fixed caps

--- views/js/controller/Foo/index.js
+++ views/js/controller/Foo/index.js
@@ -6,8 +6,8 @@ define([], function(){
             //foo starts
             window.console.log('foobar baz noz', arguments);
             window.console.warn('Ooops');
-            window.console.log('that is a cool new feature');
-            window.console.log('now the feature is complete');
+            window.console.log('That is a cool new feature');
+            window.console.log('Now the feature is complete');
         }
     };
 });
