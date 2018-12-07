41ec014402c3f43d13f5edbcd03ea1dfa5e25678
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-12-07 15:56:00 +0100

    version bump

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return [
     'label' => 'FooBar power extension',
     'description' => 'Dummy test extension',
     'license' => 'GPL-2.0',
-    'version' => '1.1.0',
+    'version' => '1.2.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=21.0.1'
