d20290da21c91d75d9cadb0acd29c4a617fdd192
Ferenc Kiss <ferenc@taotesting.com>
2019-01-08 18:44:54 +0100

    ForceTask by the Padawan - Ferenc Kiss

--- controller/TaoForce.php
+++ controller/TaoForce.php
@@ -1,35 +1,36 @@
 <?php
-/*
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * Copyright (c) 2017 Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
+ * 
+ * Copyright (c) 2019 (original work) Open Assessment Technologies SA;
+ *               
+ * 
  */
+
+namespace oat\taoForce\controller;
 
 /**
- * RAW Bootstraping
+ * @author Open Assessment Technologies SA - Ferenc Kiss
+ * @package taoForce
+ * @license GPL-2.0
  *
- * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  */
-if(PHP_SAPI == 'cli'){
-	$_SERVER['HTTP_HOST'] = 'http://localhost';
-	$_SERVER['DOCUMENT_ROOT'] = dirname(__FILE__).'/../../..';
+class TaoForce extends \tao_actions_CommonModule {
+
+    public function templateExample() {
+        $this->setData('message', 'May the Force be with you');
+        $this->setView('TaoForce/templateExample.tpl');
+    }
 }
-
-require_once dirname(__FILE__). '/../../tao/includes/class.Bootstrap.php';
-
-$bootStrap = new BootStrap('taoFooBar');
-$bootStrap->start();
-?>
