04c08264fdf88759e40b3d962eb1743dbde15fe0
Michael Garrez <michaelgarrez@Michaels-MBP.lan>
2019-01-08 15:30:31 +0100

    Replace files for taoForce

--- manifest.php
+++ manifest.php
@@ -1,65 +1,56 @@
 <?php
-/**
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * Copyright (c) 2017 Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
- */
+ * 
+ * Copyright (c) 2019 (original work) Open Assessment Technologies SA;
+ *               
+ * 
+ */               
 
-$extpath = dirname(__FILE__).DIRECTORY_SEPARATOR;
-$taopath = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'tao'.DIRECTORY_SEPARATOR;
-
-return [
-    'name' => 'taoFooBar',
-    'label' => 'FooBar power extension',
-    'description' => 'Dummy test extension',
+return array(
+    'name' => 'taoForce',
+	'label' => 'taoForce',
+	'description' => 'description',
     'license' => 'GPL-2.0',
-    'version' => '1.3.1',
-    'author' => 'Open Assessment Technologies, CRP Henri Tudor',
-    'requires' => [
-        'tao' => '>=21.0.1'
-    ],
-    'models' => [
-        'http://www.tao.lu/Ontologies/TAOFooBar.rdf'
-    ],
-    'install' => [],
-    'update' => "oat\\taoFooBar\\scripts\\update\\Updater",
-    'managementRole' => 'http://www.tao.lu/Ontologies/TAO.rdf#FooBarsManagerRole',
-    'acl' => [
-        ['grant', 'http://www.tao.lu/Ontologies/TAOFooBar.rdf#FooBarsManagerRole', ['ext'=>'taoFooBar']]
-    ],
-    'routes' => [
-        '/taoFooBar' => 'oat\\taoFooBar\\controller'
-    ],
-    'constants' => [
-        # actions directory
-        "DIR_ACTIONS"         => $extpath."actions".DIRECTORY_SEPARATOR,
-
-        # views directory
-        "DIR_VIEWS"           => $extpath."views".DIRECTORY_SEPARATOR,
-
-        # default module name
-        'DEFAULT_MODULE_NAME' => 'FooBar',
-
-        #default action name
-        'DEFAULT_ACTION_NAME' => 'index',
-
-        #BASE PATH: the root path in the file system (usually the document root)
-        'BASE_PATH'           => $extpath,
-
-        #BASE URL (usually the domain root)
-        'BASE_URL'            => ROOT_URL	.'taoFooBar/',
-    ]
-];
+    'version' => '1.0.0',
+	'author' => 'Open Assessment Technologies SA',
+	'requires' => array(
+        'tao' => '>=5.7.0'
+    ),
+	'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoForceManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoForceManager', array('ext'=>'taoForce')),
+    ),
+    'install' => array(
+    ),
+    'uninstall' => array(
+    ),
+    'routes' => array(
+        '/taoForce' => 'oat\\taoForce\\controller'
+    ),    
+	'constants' => array(
+	    # views directory
+	    "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+	    
+		#BASE URL (usually the domain root)
+		'BASE_URL' => ROOT_URL.'taoForce/',
+	    
+	    #BASE WWW required by JS
+	    'BASE_WWW' => ROOT_URL.'taoForce/views/'
+	),
+    'extra' => array(
+        'structures' => dirname(__FILE__).DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.'structures.xml',
+    )
+);
