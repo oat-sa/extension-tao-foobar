106fc77f1ae647e472bef84b8c4a22dd024c6d84
Anshul Sharma <anshulsharma@Anshuls-MacBook-Pro.local>
2019-04-03 14:18:25 +0200

    yet another console log

--- views/js/controller/Foo/foodex.js
+++ views/js/controller/Foo/foodex.js
@@ -14,6 +14,7 @@ define([], function(){
             window.console.warn('Ooops');
             window.console.log('That is a cool new feature');
             window.console.log('Now the feature is complete');
+            window.console.log('yet another console log...');
 
             window.console.log('Never use alerts, they block the event loop');
             window.console.log('that was really nasty :-O');
