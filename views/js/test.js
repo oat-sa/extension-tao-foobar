1d5309a3af15ac9931f081e70571d057414808c2
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2020-12-09 22:32:03 +0100

    test: remove duplicate

--- views/js/test.js
+++ views/js/test.js
@@ -3,5 +3,4 @@
 // breacking feature
 // one more breacking feature
 
-// fix build chore ci docs style refactor perf test feat breaking feat
-// fix build chore ci docs style refactor perf test feat breaking feat
+// fix build cre ci docs style refactor perf test feat breaking feat
